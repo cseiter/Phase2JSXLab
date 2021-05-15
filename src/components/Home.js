dee851bdc4a792966b36a1f0d6f0366c36935c50
chris <cseiter@gmail.com>
2021-05-15 18:36:22 -0400

    Home component inline style object added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  const styleObject = {
+    color: "firebrick",
+  }
   // update the JSX being returned!
-  return <div id="home"><h1>{name} is a Web Developer from {city}</h1></div>;
+  return <div id="home"><h1 style={styleObject}>{name} is a Web Developer from {city}</h1></div>;
 }
 
 export default Home;
